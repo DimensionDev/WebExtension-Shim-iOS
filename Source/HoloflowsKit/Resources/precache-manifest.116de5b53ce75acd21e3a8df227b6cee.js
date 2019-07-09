f82694b844379b01ac5e57a6b06303a25bcdff58
CMK <cirno.mainasuk@gmail.com>
2019-07-09 10:39:15 +0800

    chore: import Maskbook for testing

---
+++ Source/HoloflowsKit/Resources/precache-manifest.116de5b53ce75acd21e3a8df227b6cee.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "134ba61e298ed73e5b4ed960234e946b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f62f2397308be9df5bd2",
+    "url": "/js/4.chunk.js"
+  },
+  {
+    "revision": "252b8e3e142b00fb7347",
+    "url": "/js/5.chunk.js"
+  },
+  {
+    "revision": "5a859faa21f2bf72ba79",
+    "url": "/js/app.js"
+  },
+  {
+    "revision": "87eca0f9bdaf036e1b59",
+    "url": "/js/backgroundservice.js"
+  },
+  {
+    "revision": "d19626d9ea276e5df0df",
+    "url": "/js/contentscript.js"
+  },
+  {
+    "revision": "e6bfdb29f604ffa4993e",
+    "url": "/js/injectedscript.js"
+  },
+  {
+    "revision": "c6a80d52725b68e5480aeaa5cd69dc05",
+    "url": "/static/media/1a2.c6a80d52.jpg"
+  },
+  {
+    "revision": "fe690641e286546c0bc492f3c1211dba",
+    "url": "/static/media/1a3.fe690641.jpg"
+  }
+]);
