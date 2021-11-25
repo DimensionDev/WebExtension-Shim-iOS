3a7edff4decdef34dcdcd9d0457e7cf830759d63
jk234ert <handan0524@gmail.com>
2021-11-25 14:40:26 +0800

    feat: optimize tab+sendMessage effiency by putting it in a custom serial queue instead of main queue

--- Source/WebExtension-Shim/Classes/Tab/Tab+MessageExchange.swift
+++ Source/WebExtension-Shim/Classes/Tab/Tab+MessageExchange.swift
@@ -14,6 +14,7 @@ extension Tab {
     open func sendMessage(id: String, messageBody: String) {
         let meta = self.meta
         let url = webView.url?.absoluteString
+        let completionHandler = completionHandler()
         
         exchangeMessageQueue.async { [weak self] in
             guard let `self` = self else { return }
@@ -27,19 +28,13 @@ extension Tab {
                 let onMessage = WebExtension.OnMessage(fromMessageSender: sender, sendMessage: sendMessage)
                 let request = HoloflowsRPC.ServerRequest(params: onMessage, id: id)
                 if let tabID = sendMessage.tabId {
-                    DispatchQueue.main.async { [weak self] in
-                        guard let `self` = self else { return }
                         if let targetTab = self.browser?.tabs.storage.first(where: { $0.id == tabID }) {
-                            HoloflowsRPC.dispathRequest(webView: targetTab.webView, request: request, completionHandler: self.completionHandler())
+                            HoloflowsRPC.dispathRequest(webView: targetTab.webView, request: request, completionHandler: completionHandler)
                         } else {
                             let result: Result<HoloflowsRPC.Response<WebExtension.OnMessage>, RPC.Error> = .failure(RPC.Error.invalidParams)
-                            HoloflowsRPC.dispatchResponse(webView: self.webView, id: id, result: result, completionHandler: self.completionHandler())
+                            HoloflowsRPC.dispatchResponse(webView: self.webView, id: id, result: result, completionHandler: completionHandler)
                         }
-                    }
                 } else {
-                    DispatchQueue.main.async { [weak self] in
-                        guard let `self` = self else { return }
-
                         if let browser = self.browser {
                             var allTabs = [Tab]()
                             allTabs.append(contentsOf: browser.tabs.storage)
@@ -47,21 +42,17 @@ extension Tab {
 
                             for targetTab in allTabs {
                                 guard targetTab.id != self.id else { continue }
-                                HoloflowsRPC.dispathRequest(webView: targetTab.webView, request: request, completionHandler: self.completionHandler())
+                                HoloflowsRPC.dispathRequest(webView: targetTab.webView, request: request, completionHandler: completionHandler)
                             }
                         }
 
                         let result: Result<HoloflowsRPC.Response<String>, RPC.Error> = .success(.init(result: "", id: id))
-                        HoloflowsRPC.dispatchResponse(webView: self.webView, id: id, result: result, completionHandler: self.completionHandler())
-                    }
+                        HoloflowsRPC.dispatchResponse(webView: self.webView, id: id, result: result, completionHandler: completionHandler)
                 }
                 
             case let .failure(error):
-                DispatchQueue.main.async { [weak self] in
-                    guard let `self` = self else { return }
                     let result: Result<HoloflowsRPC.Response<WebExtension._Echo>, RPC.Error> = .failure(error)
-                    HoloflowsRPC.dispatchResponse(webView: self.webView, id: id, result: result, completionHandler: self.completionHandler())
-                }
+                    HoloflowsRPC.dispatchResponse(webView: self.webView, id: id, result: result, completionHandler: completionHandler)
             }   // end switch
         }
     }
